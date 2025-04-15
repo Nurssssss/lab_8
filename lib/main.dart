d1dd27d3dd410a4b328ec6f7fb412375db9da866
Nurssssss <joshdan1623@gmail.com>
2025-04-15 22:16:41 +0500

    lab_9

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/post/bloc/post_bloc.dart';
 import 'package:flutter_application_1/post/pages/post_screen.dart';
 import 'package:flutter_application_1/profile/pages/profile_page.dart';
+import 'package:flutter_application_1/profile/pages/start_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
@@ -20,7 +21,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => PostBloc()..add(GetPostEvent()),
-      child: MaterialApp(home: ProfilePage()),
+      child: MaterialApp(home: StartPage()),
     );
   }
 }
---
+++ lib/profile/pages/main_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/profile/pages/fourth_page.dart';
+import 'package:flutter_application_1/profile/pages/profile_page.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    Center(child: Text('Главная')),
+    Center(child: FourthPage()),
+    Center(child: ProfilePage()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Добро пожаловать")),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Главная'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Профиль'),
+          BottomNavigationBarItem(icon: Icon(Icons.library_add), label: "Repo"),
+        ],
+      ),
+    );
+  }
+}
